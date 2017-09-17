f72f9d67c8f2b76914d256d0f0a7804ba08e31f2
Gary Lai <garylaiph@gmail.com>
2017-09-18 09:38:18 +1200

    added italics for text editing

--- init.el
+++ init.el
@@ -73,7 +73,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(italic ((t (:underline t)))))
 (load-theme 'darktooth t)
 ;; toggle comment
 (defun toggle-comment-on-line ()
