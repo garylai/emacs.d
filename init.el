c24943c9d7107449962005404e5d19796f78b296
Gary Lai <garylai@Garys-MacBook-Pro-57814.local>
2017-11-13 15:26:46 +1300

    Set to use the eslint in node_modules

--- init.el
+++ init.el
@@ -202,3 +202,14 @@
 (add-hook 'js2-mode-hook #'linum-mode)
 (add-hook 'sh-mode-hook #'linum-mode)
 
+ 
+(defun my/use-eslint-from-node-modules ()
+  (let ((root (locate-dominating-file
+               (or (buffer-file-name) default-directory)
+               (lambda (dir)
+                 (let ((eslint (expand-file-name "node_modules/eslint/bin/eslint.js" dir)))
+                  (and eslint (file-executable-p eslint)))))))
+    (when root
+      (let ((eslint (expand-file-name "node_modules/eslint/bin/eslint.js" root)))
+        (setq-local flycheck-javascript-eslint-executable eslint)))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
