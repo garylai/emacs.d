4b8a06b605b2a371e7fcf03dbe423d089634ce50
Gary Lai <garylaiph@gmail.com>
2019-08-12 13:29:26 +1200

    clean up

--- configs/ivy-config.el
+++ configs/ivy-config.el
@@ -1,16 +1,14 @@
 (use-package ivy
   :straight t
-  :config
-  (progn
-    (ivy-mode))
+  :demand
+  :config (ivy-mode)
   :bind (("C-c C-r" . ivy-resume))
-  :init
-  (setq ivy-re-builders-alist
+  :custom
+  (ivy-count-format "(%d/%d) ")
+  (ivy-use-virtual-buffers t)
+  (ivy-re-builders-alist
         '((counsel-M-x . ivy--regex-fuzzy)
-          (t . ivy--regex-ignore-order )))
-  (setq
-   ivy-use-virtual-buffers t
-   ivy-count-format "(%d/%d) "))
+          (t . ivy--regex-ignore-order ))))   
 
 (use-package swiper
   :straight t
