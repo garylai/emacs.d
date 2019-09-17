e78498b2edc6162b855d22083d4bea49d719c2f7
Gary Lai <garylaiph@gmail.com>
2019-09-18 10:56:56 +1200

    open agenda in current winder

--- configs/org-config.el
+++ configs/org-config.el
@@ -6,10 +6,12 @@
   :mode (("\\.org$" . org-mode))
   :init
   (setq org-log-done t)
-  (add-hook 'org-shiftup-final-hook #'windmove-up)
-  (add-hook 'org-shiftleft-final-hook #'windmove-left)
-  (add-hook 'org-shiftdown-final-hook #'windmove-down)
-  (add-hook 'org-shiftright-final-hook #'windmove-right)
+  (setq org-agenda-window-setup (quote current-window))
+  (setq org-src-window-setup (quote current-window))
+  ;; (add-hook 'org-shiftup-final-hook #'windmove-up)
+  ;; (add-hook 'org-shiftleft-final-hook #'windmove-left)
+  ;; (add-hook 'org-shiftdown-final-hook #'windmove-down)
+  ;; (add-hook 'org-shiftright-final-hook #'windmove-right)
   (setq org-agenda-files (list "~/org"
                                "~/org/1on1"))
   :bind
