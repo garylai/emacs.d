f3ac886095112e53f5b65e865c91e71926f89e77
Gary Lai <garylaiph@gmail.com>
2020-07-07 16:17:44 +1200

    start groovy mode on jenkinsfile

--- configs/groovy-config.el
+++ configs/groovy-config.el
@@ -2,6 +2,13 @@
   (require 'use-package))
 
 (use-package groovy-mode
-	      :straight t)
+  :straight t
+  :hook
+  (groovy-mode . show-paren-mode)
+  (groovy-mode . hs-minor-mode)
+  (groovy-mode . hl-line-mode)
+  (groovy-mode . display-line-numbers-mode)
+  :mode ("\\[j|J]enkinsfile$" . groovy-mode)
+  )
 
 (provide 'groovy-config)
