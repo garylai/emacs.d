da327842a1281769a263ed41e4ee523c401f4769
Gary Lai <garylaiph@gmail.com>
2020-07-13 21:54:14 +1200

    increase vterm scroll back

--- configs/vterm-config.el
+++ configs/vterm-config.el
@@ -6,6 +6,7 @@
   :demand
   :config
   (setq vterm-buffer-name-string "%s"
+        vterm-max-scrollback 100000
         vterm-kill-buffer-on-exit t)
   (custom-set-faces
    '(vterm-color-default ((t (:inherit default :background "black")))))
