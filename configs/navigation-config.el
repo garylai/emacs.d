a2b1626d67056d448b4d5b64b35c82680b687c3f
Gary Lai <garylaiph@gmail.com>
2020-07-16 17:22:31 +1200

    Replaced Neotree with Treemacs

--- configs/navigation-config.el
+++ configs/navigation-config.el
@@ -18,13 +18,36 @@
   :bind (:map projectile-mode-map
 	 ("C-c p" . projectile-command-map)))
 
-(use-package neotree
+;; (use-package neotree
+;;   :straight t
+;;   :bind ([f8] . neotree-toggle)
+;;   :config
+;;   (setq neo-autorefresh nil)
+;;   (setq neo-smart-open t)
+;;   )
+
+
+(use-package treemacs
   :straight t
-  :bind ([f8] . neotree-toggle)
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (setq neo-autorefresh nil)
-  (setq neo-smart-open t)
+  (progn
+    (setq aw-ignored-buffers (delq 'treemacs-mode aw-ignored-buffers)
+          treemacs-no-png-images nil
+          treemacs-space-between-root-nodes nil
+          treemacs-width 30
+          treemacs-resize-icons 15)
+    (treemacs-follow-mode t))
+  :bind (([f8] . treemacs))
+  (:map treemacs-mode-map
+        ("C-s" . treemacs-find-file))
   )
 
+(use-package treemacs-projectile
+  :straight t
+  :after treemacs projectile
+  :ensure t)
 
 (provide 'navigation-config)
