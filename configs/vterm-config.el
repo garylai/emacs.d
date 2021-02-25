d3dbd7c4db7646958f4ebd83f549e995cc6141df
Gary Lai <garylaiph@gmail.com>
2021-02-25 16:39:18 +1300

    fixed vterm color

--- configs/vterm-config.el
+++ configs/vterm-config.el
@@ -9,7 +9,7 @@
         vterm-max-scrollback 100000
         vterm-kill-buffer-on-exit t)
   (custom-set-faces
-   '(vterm-color-default ((t (:inherit default :background "black")))))
+   '(vterm-color-black ((t (:background "gray72" :foreground "black")))))
   :bind
   (("C-c v n" . vterm-other-window)
    ("C-c v v" . vterm))
