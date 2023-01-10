149cf3fe5624c7caeecf0cfb81d430b84f49ebcb
Gary Lai <garylaiph@gmail.com>
2023-01-11 10:11:09 +1300

    adding more minor mode to csharp mode

--- configs/csharp-config.el
+++ configs/csharp-config.el
@@ -2,8 +2,15 @@
   (require 'use-package))
 
 ;; install dotnet by installer https://dotnet.microsoft.com/en-us/download/dotnet
-;; install Omnisharp-Roslyn language server by lsp-install-server
+;; *** install Omnisharp-Roslyn language server by lsp-install-server
+;; use csharp-ls instead; Omnisharp-Roslyn doesn't support go to meta data and they don't care
 (use-package csharp-mode
-	      :straight t)
+  :straight t
+  :hook
+  (csharp-mode . show-paren-mode)
+  (csharp-mode . hs-minor-mode)
+  (csharp-mode . hl-line-mode)
+  (csharp-mode . display-line-numbers-mode)
+)
 
 (provide 'csharp-config)
