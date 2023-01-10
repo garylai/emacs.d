aac7dae96485bc7c0e6c966a80e31757033781f6
Gary Lai <garylaiph@gmail.com>
2023-01-10 15:43:18 +1300

    try using eterm

--- configs/vterm-config.el
+++ configs/vterm-config.el
@@ -1,11 +1,22 @@
 (eval-when-compile
   (require 'use-package))
 
+;; (use-package eterm-256color
+;;   :straight t
+;;   :config
+;;   (custom-set-faces
+;;    '(eterm-256color-black ((t :inherit default))))
+;;   )
+
 (use-package vterm
   :straight t
   :demand
+  ;; :hook
+  ;; (vterm-mode . eterm-256color-mode)
   :config
-  (setq vterm-buffer-name-string "%s"
+  (setq
+   ;; vterm-term-environment-variable "eterm-color"
+        vterm-buffer-name-string "%s"
         vterm-max-scrollback 100000
         vterm-kill-buffer-on-exit t)
   (custom-set-faces
