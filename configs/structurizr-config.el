339467d4570b808763f056de28fa918cd1a8edcc
Gary Lai <gary.lai@medenterprises.com>
2024-06-20 23:05:36 +1200

    added structurizr mode

---
+++ configs/structurizr-config.el
@@ -0,0 +1,7 @@
+(eval-when-compile
+  (require 'use-package))
+
+(use-package structurizr-mode
+  :quelpa (structurizr-mode :fetcher github :repo "gilesp/structurizr-mode"))
+
+(provide 'structurizr-config)
